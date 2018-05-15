66e42d3cfd2b363e8fbdf33f5d3a812d88c67929
kensley <klp.github@gmail.com>
2018-05-15 11:46:49 -0400

    corrected a path for the ithub assistant admin script

--- inc/enqueue_admin.php
+++ inc/enqueue_admin.php
@@ -14,6 +14,6 @@ function ithub_enqueue_admin($hook_suffix) {
     $js_version = $theme_version . '.' . filemtime(get_template_directory() . '/js/theme.min.js');
     wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), $js_version, true );
     wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), $the_theme->get( 'Version' ), true );
-    wp_enqueue_script( 'ithub-scripts', plugin_dir_url( __FILE__ ).'js/ithub_assist_admin.js',false,'1.0.0',true);
+    wp_enqueue_script( 'ithub-scripts', plugin_dir_url( __FILE__ ).'../js/ithub_assist_admin.js',false,'1.0.0',true);
     }
 }
